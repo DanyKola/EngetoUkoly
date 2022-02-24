d7d816d2e77a5050b094d17bfa59abc7cc82636a
DanyKola <me@danielakolarikova.cz>
2022-02-24 14:26:52 +0100

    Domaci ukol z 1 lekce, zakomponovane pripominky  Changes to be committed: 	new file:   lekce01/Promenne.java 	new file:   lekce01/opravChyby.java

---
+++ lekce01/Promenne.java
@@ -0,0 +1,18 @@
+package com.company;
+
+import java.time.LocalDate;
+
+public class Main {
+
+            public static void main(String[] args) {
+                String jmenoPrijmeni = "Jiří Novák";
+                int pocetNakupu = 5;
+                String nazevMesta = "Kladno";
+                double spotrebaVozidla = 5.1;
+                double delkaKabelu = 3.6;
+                LocalDate datumNarozeni = LocalDate.of(1987,3,24 );
+                String spz = "3B3 3333";
+                String ipAdresa = "192.168.48.39";
+
+            }
+        }
